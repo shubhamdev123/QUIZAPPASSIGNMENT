f188e64bf1fee5c281ade7f4daabb6d931fe7fe1
shubhamdev123 <shubham.dev123int@gmail.com>
2024-02-09 14:54:35 +0530

    Files added

---
+++ Assessment/data/common-knowledge.js
@@ -0,0 +1,209 @@
+const quizData = {
+  title: "Quize app Application"
+};
+
+const questions = [
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is my favourite place ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  },
+  {
+    text: "What is my favourite place ?",
+    type: "mc",
+    answers: [
+      { text: "Elon", correct: true },
+      { text: "Jeff", correct: false },
+      { text: "Rocky", correct: false},
+      { text: "Roman", correct: false }
+    ]
+  }
+
+];
+
+module.exports = { quizData, questions };
